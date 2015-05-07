747adc7bc5f1a10bcc0842c9c40ec2f671d76f3e
Zaid <zaid@arkverse.com>
2015-05-07 16:29:57 -0700

    Initial Commit

---
+++ SwiftRecord/SwiftRecord.h
@@ -0,0 +1,13 @@
+//
+//  SwiftRecord.h
+//  SwiftRecord
+//
+//  Created by Zaid on 5/7/15.
+//  Copyright (c) 2015 ark. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SwiftRecord : NSObject
+
+@end
---
+++ SwiftRecord/SwiftRecord.m
@@ -0,0 +1,13 @@
+//
+//  SwiftRecord.m
+//  SwiftRecord
+//
+//  Created by Zaid on 5/7/15.
+//  Copyright (c) 2015 ark. All rights reserved.
+//
+
+#import "SwiftRecord.h"
+
+@implementation SwiftRecord
+
+@end
