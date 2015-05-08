a5324b747e90bb3193bba811a2b2663c52aa6165
Zaid <zaid@arkverse.com>
2015-05-07 18:49:45 -0700

    start

---
+++ Mac/SwiftRecord/SwiftRecord/SwiftRecord.h
@@ -0,0 +1,19 @@
+//
+//  SwiftRecord.h
+//  SwiftRecord
+//
+//  Created by Zaid on 5/7/15.
+//  Copyright (c) 2015 ark. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftRecord.
+FOUNDATION_EXPORT double SwiftRecordVersionNumber;
+
+//! Project version string for SwiftRecord.
+FOUNDATION_EXPORT const unsigned char SwiftRecordVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftRecord/PublicHeader.h>
+
+
